ed12ab5585d7cf0f32eeffbd4d7d14b514115596
youlan_lan <youlan_lan@qq.com>
2022-06-06 00:46:03 +0800

    feat: 公共卡片组件初版

---
+++ components/card-item/card-item.js
@@ -0,0 +1,16 @@
+// components/card-item.js
+Component({
+  properties:{
+    detail: {
+      id: 1,
+      title: "",
+      info: "",
+      deadline: '',
+    }
+  },
+
+  data: {
+    
+  },
+
+})
